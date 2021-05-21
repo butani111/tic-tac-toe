81e52baa0954f98c641b47a8f4af494c86a3ff1d
Shyam <butanishyam111@gmail.com>
2021-05-21 11:17:55 +0530

    Changes in responsiveness, box-design
    Highlight the pattern when user win, hover effect added

--- script.js
+++ script.js
@@ -7,23 +7,25 @@
 let boxes = document.querySelectorAll('#screen span') // squares
 let userNameBoxes = document.querySelectorAll('#user-name span')
 let userTurn = 0
-let noOfLines = 3                                    // Change no. of squares (n x n)
-let totalUser = 2;                                   // Change no. of users
-let symbols = ['X', 'O']                             // Add/Remove symbols as no. of users changes
-let userNameColor = ['rgb(137, 218, 137)', 'white']  // Color of box which indicates that whose turn is it now
+let noOfLines = 3 // Change no. of squares (n x n)
+let totalUser = 2; // Change no. of users
+let symbols = ['X', 'O'] // Add/Remove symbols as no. of users changes
+let userNameColor = ['rgb(137, 218, 137)', 'white'] // Color of box which indicates that whose turn is it now
+let boxInitialBgColor = 'white'
+let boxColorOnWinning = 'rgb(137, 218, 137)'
 let turnsSoFar = 0
 
 // Declare Onclick function for all squares
 
 for (let i = 0; i < boxes.length; i++) {
-  boxes[i].onclick = function(e) {
-    if(this.innerHTML == ''){
+  boxes[i].onclick = function (e) {
+    if (this.innerHTML == '') {
 
       this.innerHTML = symbols[userTurn]
       userNameBoxes[userTurn].style.backgroundColor = userNameColor[1]
 
       boxContent = []
-      for(let it = 0; it < boxes.length; it++) {
+      for (let it = 0; it < boxes.length; it++) {
         boxContent[it] = boxes[it].innerHTML
       }
 
@@ -31,68 +33,82 @@ for (let i = 0; i < boxes.length; i++) {
       var gameFinished = false
 
       // check row
-      for (let it = 0; it < noOfLines; it++) {
+      for (let it = 0; !gameFinished && it < noOfLines; it++) {
         var flag = true
-        if(boxContent[it*noOfLines] == '') continue
-        for (let it1 = 0; it1+1 < noOfLines; it1++) {
-          if(boxContent[it*noOfLines + it1] != boxContent[it*noOfLines + it1+1]) {
+        if (boxContent[it * noOfLines] == '') continue
+        for (let it1 = 0; it1 + 1 < noOfLines; it1++) {
+          if (boxContent[it * noOfLines + it1] != boxContent[it * noOfLines + it1 + 1]) {
             flag = false
             break
           }
         }
-        if(flag) {
+        if (flag) {
           gameFinished = true
-          break
+          for (let it1 = 0; it1 < noOfLines; it1++) {
+            boxes[it * noOfLines + it1].style.backgroundColor = boxColorOnWinning
+          }
         }
       }
 
       // check colomn
-      for (let it = 0; it < noOfLines; it++) {
+      for (let it = 0; !gameFinished && it < noOfLines; it++) {
         var flag = true
-        if(boxContent[it] == '') continue
-        for (let it1 = 0; it1+1 < noOfLines; it1++) {
-          if(boxContent[it + noOfLines*it1] != boxContent[it + noOfLines*(it1+1)]) {
+        if (boxContent[it] == '') continue
+        for (let it1 = 0; it1 + 1 < noOfLines; it1++) {
+          if (boxContent[it + noOfLines * it1] != boxContent[it + noOfLines * (it1 + 1)]) {
             flag = false
             break
           }
         }
-        if(flag) {
+        if (flag) {
           gameFinished = true
-          break
+          for (let it1 = 0; it1 < noOfLines; it1++) {
+            boxes[it + noOfLines * it1].style.backgroundColor = boxColorOnWinning
+          }
         }
       }
 
       // check diagonal
       var flag = (boxContent[0] != '') //true
-      for (let it = 0; it+1 < noOfLines; it++) {
-        if(boxContent[it*noOfLines + it] != boxContent[(it+1)*noOfLines + it+1]) {
+      for (let it = 0; !gameFinished && it + 1 < noOfLines; it++) {
+        if (boxContent[it * noOfLines + it] != boxContent[(it + 1) * noOfLines + it + 1]) {
           flag = false
           break
         }
       }
-      if(flag) gameFinished = true
-      
+      if (!gameFinished && flag) {
+        gameFinished = true
+        for (let it = 0; it < noOfLines; it++) {
+          boxes[it * noOfLines + it].style.backgroundColor = boxColorOnWinning
+        }
+      }
+
       var flag = (boxContent[noOfLines - 1] != '') //true
-      for (let it = 0; it+1 < noOfLines; it++) {
-        if(boxContent[it*noOfLines + (noOfLines - it - 1)] != boxContent[(it+1)*noOfLines + (noOfLines - it - 2)]) {
+      for (let it = 0; !gameFinished && it + 1 < noOfLines; it++) {
+        if (boxContent[it * noOfLines + (noOfLines - it - 1)] != boxContent[(it + 1) * noOfLines + (noOfLines - it - 2)]) {
           flag = false
           break
         }
       }
-      if(flag) gameFinished = true
+      if (!gameFinished && flag) {
+        gameFinished = true
+        for (let it = 0; it < noOfLines; it++) {
+          boxes[it * noOfLines + (noOfLines - it - 1)].style.backgroundColor = boxColorOnWinning
+        }
+      }
 
       // If current user compeleted pattern, diclare the result
-      if(gameFinished) {
+      if (gameFinished) {
         document.getElementById('msg-on-top').innerHTML = userNameBoxes[userTurn].innerHTML + ' is winner !!'
         return
       }
 
       turnsSoFar += 1
-      if(turnsSoFar >= noOfLines*noOfLines) {
+      if (turnsSoFar >= noOfLines * noOfLines) {
         document.getElementById('msg-on-top').innerHTML = 'Tie !!, It seems like you both are good at this.'
       }
 
-      userTurn = (userTurn+1) % totalUser;
+      userTurn = (userTurn + 1) % totalUser;
       userNameBoxes[userTurn].style.backgroundColor = userNameColor[0]
     }
   }
@@ -100,9 +116,10 @@ for (let i = 0; i < boxes.length; i++) {
 
 // Onclick funcion for New-game-btn
 
-document.getElementById('new-game-btn').onclick = function(e) {
+document.getElementById('new-game-btn').onclick = function (e) {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].innerHTML = '';
+    boxes[i].style.backgroundColor = boxInitialBgColor
   }
   turnsSoFar = 0
   document.getElementById('msg-on-top').innerHTML = ''
